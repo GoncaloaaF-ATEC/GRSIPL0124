f19f895a3aefa4dcf0bbe81c49db1a745cd3d9ef
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-07-25 10:01:08 +0100

    demo var, concat str e if

---
+++ UFCD 5118 - Programação orientada a objetos/1_intro/intro.py
@@ -0,0 +1,71 @@
+print("Ola Mundo")
+
+nome = "Valor" # str
+print(nome)
+
+'''
+comnet 
+multi
+linha
+'''
+
+idade = 10 # int
+ano = 2031
+
+soma = idade + ano
+
+print(soma)
+
+
+media = soma / 2
+
+print(media)
+
+soma2 = media + 20
+
+print(type(soma2))
+
+#max int  java -> 2 147 483 647
+
+soma2 = "sem valor"
+
+print(type(soma2))
+
+idade = 20
+print("----prt1----")
+print("nome: " + nome + "\nidade: " + str(idade))
+print("----prt2----")
+print(f"nome: {nome} \nidade: {idade}")
+
+# condições -> if,  "swith"
+print("----if----")
+val = 2
+
+if val == 1:
+    print("val = 1")
+    print("ainda dentro do if")
+elif val == 2:
+    print("val = 2")
+    print("ainda dentro do elif (else if)")
+else:
+    print("val != 1")
+    print("ainda dentro do else")
+
+
+print("Fora do if")
+
+
+
+
+# loops -> for, while
+
+
+
+
+
+# func -> funções
+
+# classes
+
+# herança
+
