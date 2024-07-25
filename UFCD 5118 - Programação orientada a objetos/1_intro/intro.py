00a145b95d30aefbbec0be0aa3fdaee22580b637
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-07-25 11:46:18 +0100

    match, range, for, while, funcs (def)

--- UFCD 5118 - Programação orientada a objetos/1_intro/intro.py
+++ UFCD 5118 - Programação orientada a objetos/1_intro/intro.py
@@ -1,6 +1,6 @@
 print("Ola Mundo")
 
-nome = "Valor" # str
+nome = "Valor"  # str
 print(nome)
 
 '''
@@ -9,13 +9,12 @@ multi
 linha
 '''
 
-idade = 10 # int
+idade = 10  # int
 ano = 2031
 
 soma = idade + ano
 
 print(soma)
-
 
 media = soma / 2
 
@@ -51,21 +50,111 @@ else:
     print("val != 1")
     print("ainda dentro do else")
 
-
 print("Fora do if")
 
+print("----swith - match----")
+
+val = 145678
+
+match val:
+    case 1:
+        print("val = 1")
+    case 5:
+        print("val = 5")
+    case 10:
+        print("val = 10")
+    case _:
+        print("outro valor")
+
+print(" condição \"complexas\" ")
+
+# && <- and
+# || <- or
 
 
+v1 = True
+v2 = False
+
+print(v1 or v2)
+print(v1 and v2)
+
+val1 = 10
+val2 = 20
+print(not (val2 > 10 and val1 < 50))
 
 # loops -> for, while
 
+print("----range----")
 
+r1 = range(0, 10, 2)  # range(lb, ub, step) ->  lb ate ub-1, de step em step, se o step for omitido step = 1
 
+# for(val in ...)
+print("----for range----")
+for i in r1:
+    print(i)
+
+print("----for str----")
+for i in "lorem ipsum":
+    print(i)
+
+print("----while----")
+val = 100
+while val > 0:
+    print(val)
+    val -= 1
+
+print("--------------for v2 ------------")
+
+for i in range(0, 1000, 10):
+
+    if i % 20 == 0:
+        continue
+
+    if i == 120:
+        break
+
+    print(i)
 
 
 # func -> funções
 
+def nome():
+    print("Ola Mundo")
+
+
+nome()
+nome()
+nome()
+
+
+def nome2(txt: str, ano: int = 1991):
+    # pass # <- bloco vazio
+    print(f"Ola {txt}, em {ano} ")
+
+
+print("------")
+nome2("Mundo", 2013)
+nome2("Gonçalo", 1231)
+nome2("bla bla bla", 41212)
+nome2("py")
+
+print("-----")
+
+
+def genMsg(nome: str, ano: int = 1991) -> str:
+    return f"Ola {nome}, em {ano}"
+
+print(genMsg("Mundo"))
+
+
+
+# lists
+
+# set
+
+#dict
+
+
 # classes
 
 # herança
-
